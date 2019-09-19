2c807b8b499e12c909f596b7ca4e0c929dc6104e
Eli Gooch <eli.gooch@centre.edu>
2019-09-19 11:03:48 -0400

    fajs

--- scrape.js
+++ scrape.js
@@ -2,4 +2,17 @@ var scraper = require('./tools').scraper;
 //scraper = function(url,parseFcn,filename)
 //scrapes a URL, parses it with parseFcn, and archives the data in a JSON file called filename.
 
+//.JCAZList-list
 
+var store = [];
+var getMarvel = function($,data){
+    var parent = $(".JCAZList-list");
+    var children = parent.find("div");
+    children.toArray().forEach(function(item,index){
+                               data.push($(item).text());
+                               });
+}
+
+scraper("https://www.marvel.com/comics/characters", getMarvel(), newStore.json);
+
+//console.log(store);
